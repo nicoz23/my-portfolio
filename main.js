863b36aeec8653f659542db76d168d241d065be8
Nicolás Zunino <77420763+nicoz23@users.noreply.github.com>
2023-01-02 11:59:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,134 @@
+
+    particlesJS(
+        {
+            "particles": {
+              "number": {
+                "value": 65,
+                "density": {
+                  "enable": true,
+                  "value_area": 850
+                }
+              },
+              "color": {
+                "value": "#ff0844"
+              },
+              "shape": {
+                "type": "circle",
+                "stroke": {
+                  "width": 0,
+                  "color": "#000000"
+                },
+                "polygon": {
+                  "nb_sides": 5
+                },
+                "image": {
+                  "src": "img/github.svg",
+                  "width": 100,
+                  "height": 100
+                }
+              },
+              "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                  "enable": false,
+                  "speed": 1,
+                  "opacity_min": 0.1,
+                  "sync": false
+                }
+              },
+              "size": {
+                "value": 4,
+                "random": true,
+                "anim": {
+                  "enable": false,
+                  "speed": 40,
+                  "size_min": 0.1,
+                  "sync": false
+                }
+              },
+              "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#fd3624",
+                "opacity": 0.4,
+                "width": 1
+              },
+              "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                  "enable": false,
+                  "rotateX": 600,
+                  "rotateY": 1200
+                }
+              }
+            },
+            "interactivity": {
+              "detect_on": "canvas",
+              "events": {
+                "onhover": {
+                  "enable": false,
+                  "mode": "bubble"
+                },
+                "onclick": {
+                  "enable": true,
+                  "mode": "repulse"
+                },
+                "resize": true
+              },
+              "modes": {
+                "grab": {
+                  "distance": 400,
+                  "line_linked": {
+                    "opacity": 1
+                  }
+                },
+                "bubble": {
+                  "distance": 400,
+                  "size": 40,
+                  "duration": 2,
+                  "opacity": 8,
+                  "speed": 3
+                },
+                "repulse": {
+                  "distance": 200,
+                  "duration": 0.4
+                },
+                "push": {
+                  "particles_nb": 4
+                },
+                "remove": {
+                  "particles_nb": 2
+                }
+              }
+            },
+            "retina_detect": true
+          }
+    )
+
+
+//  click to up
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector('.go-top-container')
+        .classList.add('show');
+    }else{
+        document.querySelector('.go-top-container')
+        .classList.remove('show');
+    }
+}
+
+document.querySelector('.go-top-container')
+.addEventListener('click', () => {
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    });
+});
